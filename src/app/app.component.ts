d6ad666c27ef8bc10934514b57765f8997110f1b
eduardo.vaz <eduardo.vaz@quark.tec.br>
2022-06-27 14:43:54 -0300

    Barra de tarefas Botão adicionar carona Botão cadastre-se

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import { AddDialogComponent } from './add-dialog/add-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Projeto-Carona';
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(AddDialogComponent, {
+      width: '30%'
+    });
+    this.dialog.afterAllClosed.subscribe(() => {
+      window.location.reload();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from "@angular/router";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { AppRoutingModule } from './app-routing.module';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { AddDialogComponent } from './add-dialog/add-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from "@angular/material/select";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDialogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
