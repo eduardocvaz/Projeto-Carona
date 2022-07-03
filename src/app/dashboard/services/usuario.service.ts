ad50507899b794f271c42c0b51ca0764aeec8f20
eduardo <eduardo.c.vaz@hotmail.com>
2022-07-02 22:24:25 -0300

    Crud realizado para usuario

---
+++ src/app/dashboard/services/usuario.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { delay, first, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private url = 'http://localhost:3000/usuario';
+
+  post(usuario: Usuario) {
+    return this.httpClient.post(this.url, usuario)
+    .pipe(
+      first(),
+      tap(() => console.log('Usuário cadastrado com sucesso!')),
+      delay(1000)
+    );
+  }
+}
